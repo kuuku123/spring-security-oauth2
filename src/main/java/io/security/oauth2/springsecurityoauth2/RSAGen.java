66d7fbc65154b1122e8e2c0ffd3df5c63479e02d
dohyun_lim <dohyun_lim@tmax.co.kr>
2022-12-23 12:43:23 +0900

    RSA

--- src/main/java/io/security/oauth2/springsecurityoauth2/RSAGen.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/RSAGen.java
@@ -1,4 +1,41 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 public class RSAGen {
+    public static KeyPair genKeyPair() throws Exception {
+        KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+        gen.initialize(1024, new SecureRandom());
+        return gen.genKeyPair();
+    }
+
+    public static String encrypt(String plainText, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        byte[] bytePlain = cipher.doFinal(plainText.getBytes());
+        return Base64.getEncoder().encodeToString(bytePlain);
+    }
+
+    public static String decrypt(String encrypted, PrivateKey privateKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        byte[] byteEncrypted = Base64.getDecoder().decode(encrypted.getBytes());
+
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] bytePlain = cipher.doFinal(byteEncrypted);
+        return new String(bytePlain, "utf-8");
+    }
+
+    public static PublicKey getPublicKeyFromKeySpec(String base64PublicKey)  throws Exception {
+        byte[] decodedBase64PubKey = Base64.getDecoder().decode(base64PublicKey);
+        return KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(decodedBase64PubKey));
+    }
+
+    public static PrivateKey getPrivateKeyFromKeySpec(String base64PrivateKey) throws Exception {
+        byte[] decodedBase64PrivateKey = Base64.getDecoder().decode(base64PrivateKey);
+        return KeyFactory.getInstance("RSA").generatePrivate(new X509EncodedKeySpec(decodedBase64PrivateKey));
+    }
 }
--- src/main/java/io/security/oauth2/springsecurityoauth2/RSATest.java
+++ src/main/java/io/security/oauth2/springsecurityoauth2/RSATest.java
@@ -1,4 +1,43 @@
 package io.security.oauth2.springsecurityoauth2;
 
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
 public class RSATest {
+
+    public static void rsa(String message) throws Exception {
+
+        KeyPair keyPair = RSAGen.genKeyPair();
+        PublicKey publicKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+        String encrypted = RSAGen.encrypt(message, publicKey);
+        String decrypted = RSAGen.decrypt(encrypted, privateKey);
+
+        System.out.println("message = " + message);
+        System.out.println("decrypted = " + decrypted);
+
+        // key spec 전환하기
+        byte[] bytePublicKey = publicKey.getEncoded();
+        String base64PublicKey = Base64.getEncoder().encodeToString(bytePublicKey);
+        byte[] bytePrivateKey = privateKey.getEncoded();
+        String base64PrivateKey = Base64.getEncoder().encodeToString(bytePrivateKey);
+
+        // X.509 표준형식
+        PublicKey X509PublicKey = RSAGen.getPublicKeyFromKeySpec(base64PublicKey);
+        String encrypted2 = RSAGen.encrypt(message, X509PublicKey);
+        String decrypted2 = RSAGen.decrypt(encrypted2, privateKey);
+
+        System.out.println("message = " + message);
+        System.out.println("decrypted2 = " + decrypted2);
+
+        // PKCS8 표준형식
+        PrivateKey PKCS8PrivateKey = RSAGen.getPrivateKeyFromKeySpec(base64PrivateKey);
+        String decrypted3 = RSAGen.decrypt(encrypted2, PKCS8PrivateKey);
+
+        System.out.println("message = " + message);
+        System.out.println("decrypted3 = " + decrypted3);
+    }
 }
